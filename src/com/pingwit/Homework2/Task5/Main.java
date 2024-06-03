84e2b7f168cb9fc0637b67a40744c89bda2358b8
Yahor Lishtvaan <gdfsdkx654@gmail.com>
2024-06-04 00:22:07 +0200

    HW 1 and 2 completed and code reformat done

---
+++ src/com/pingwit/Homework2/Task5/Main.java
@@ -0,0 +1,10 @@
+package com.pingwit.Homework2.Task5;
+
+public class Main {
+    public static void main(String[] args) {
+        int firstVariable = 1025;
+        int secondVariable = 15;
+        int result = firstVariable % secondVariable;
+        System.out.println(result);
+    }
+}
