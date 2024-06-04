cf49181f41bc4e18a5bc33dc926f0b037804863b
Alexander Fomin <rezviybelorus@gmail.com>
2024-06-04 21:33:57 +0200

    check homework 1,2

--- src/com/pingwit/homework1/task6/Main.java
+++ src/com/pingwit/homework1/task6/Main.java
@@ -1,9 +1,10 @@
 package com.pingwit.homework1.task6;
 
-public class Main {
+public class Main { // старайся для классов придумывать названия, согласно таске, например TwoNumbersMultiply, это хороший способ постепенно привыкать в хорошему именованию
     public static void main(String[] args) {
-        byte firstVar = 25;
-        int secondVar = 300;
+        // я понимаю почему ты выбрал byte, но лучше использовать int/Integer.
+        byte firstVar = 25;  // firstVar -> здесь не критично, но Var в названиях лучше не использовать. У тебя переменная хранит число, можно назвать firstNumber
+        int secondVar = 300; // см комментарий выше
         int result = firstVar * secondVar;
         System.out.println(result);
     }
--- src/com/pingwit/homework1/task7/Main.java
+++ src/com/pingwit/homework1/task7/Main.java
@@ -1,8 +1,8 @@
 package com.pingwit.homework1.task7;
 
-public class Main {
+public class Main { // ты уже знаешь что делать с этим названием
     public static void main(String[] args) {
-        int firstVar = 300;
+        int firstVar = 300; // и с этим тоже :)
         int secondVar = 24;
         int result = firstVar % secondVar;
         System.out.println(result);
