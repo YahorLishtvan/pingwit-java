4f41436b6cb51e6f99b7a19f047fd48073b81304
Yahor Lishtvaan <gdfsdkx654@gmail.com>
2024-06-05 09:14:25 +0200

    Completed HW3. Comments have been corrected.

--- src/com/pingwit/homewrk3/task1/ATM.java
+++ src/com/pingwit/homewrk3/task1/ATM.java
@@ -9,24 +9,24 @@ public class ATM {
         System.out.println("Enter 1 to change to Pound");
         System.out.println("Enter 2 to change to EURO");
         System.out.println("Enter 3 to change to BUN");// добавь сюда абзац, чтобы визуально разделить код на логические блоки
+
         Scanner scanner = new Scanner(System.in);
         int currencyId = scanner.nextInt();
         switch (currencyId) {
             case 1 -> {
-                float resultChanges = (float) (amountDollars * 6.3); // старайся избегать класс каста, очень опасная штука. float resultChanges -> double resultChanges
+                double resultChanges = amountDollars * 6.3; // старайся избегать класс каста, очень опасная штука. float resultChanges -> double resultChanges
                 System.out.println("Your amount is " + resultChanges + " zl");
             }
             case 2 -> {
-                float resultChanges = (float) (amountDollars * 4.4); // старайся избегать класс каста, очень опасная штука. float resultChanges -> double resultChanges
+                double resultChanges = amountDollars * 4.4; // старайся избегать класс каста, очень опасная штука. float resultChanges -> double resultChanges
                 System.out.println("Your amount is " + resultChanges + " zl");
             }
             case 3 -> {
-                float resultChanges = (float) (amountDollars * 0.8); // старайся избегать класс каста, очень опасная штука. float resultChanges -> double resultChanges
+                double resultChanges = amountDollars * 0.8; // старайся избегать класс каста, очень опасная штука. float resultChanges -> double resultChanges
                 System.out.println("Your amount is " + resultChanges + " zl");
             }
-            default -> { // фигурные скобки в 27 и 29 строке можно убрать, т.к. только один оператор, но для if(){} необходимо использовать всегда
+            default ->  // фигурные скобки в 27 и 29 строке можно убрать, т.к. только один оператор, но для if(){} необходимо использовать всегда
                 System.out.println("Sorry, but our bank does not work with this currency.");
-            }
         }
     }
 }
