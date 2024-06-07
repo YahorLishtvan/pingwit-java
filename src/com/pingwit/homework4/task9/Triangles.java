1ce300ae1befad1b9126e1a9b42c9746f877c1df
Yahor Lishtvan <gdfsdkx654@gmail.com>
2024-06-07 16:35:21 +0200

    Completed HW4.

--- src/com/pingwit/homework4/task9/Triangles.java
+++ src/com/pingwit/homework4/task9/Triangles.java
@@ -4,19 +4,17 @@ import java.util.Scanner;
 
 public class Triangles {
     public static void main(String[] args) {
-        System.out.print("Введите к-ство строк в ромбе: ");
-        Scanner scan = new Scanner(System.in);
-        int rhombus = scan.nextInt();
-        int center = rhombus / 2;
-        for (int i = 0; i < rhombus; i++) {
-            for (int j = 0; j < rhombus; j++) {
+        int countSides = 4;
+        int center = countSides / 2;
+        for (int i = 0; i < countSides; i++) {
+            for (int j = 0; j < countSides; j++) {
                 if (i <= center) {
                     if (j >= center - i && j <= center + i) {
                         System.out.print("*");
                     } else
                         System.out.print(" ");
                 } else {
-                    if (j >= center + i - rhombus + 1 && j <= center - i + rhombus - 1) {
+                    if (j >= center + i - countSides + 1 && j <= center - i + countSides - 1) {
                         System.out.print("*");
                     } else {
                         System.out.print(" ");
