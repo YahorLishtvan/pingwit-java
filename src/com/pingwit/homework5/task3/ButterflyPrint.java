f1cb971acc6ac07fa38755676688e99ac806f1a7
Alexander Fomin <rezviybelorus@gmail.com>
2024-07-06 15:15:33 +0200

    check homework 5

--- src/com/pingwit/homework5/task3/ButterflyPrint.java
+++ src/com/pingwit/homework5/task3/ButterflyPrint.java
@@ -2,6 +2,23 @@ package com.pingwit.homework5.task3;
 
 import java.util.Scanner;
 
+// отформатируй код
+/*
+если поставить размер бабочки 5, то будет
+1                1
+12              21
+123            321
+1234          4321
+12345        54321
+123456      654321
+12345        54321
+1234          4321
+123            321
+12              21
+1                1
+
+Таких пробелов в центре быть не должно, попробуй пока сам поискать проблему, воспользуйся дебагом
+ */
 public class ButterflyPrint {
     public static void main(String[] args) {
         System.out.println("Hello!\n"+"I'll draw you a butterfly!\n"+"Write in a number how big you want it to be.");
@@ -31,8 +48,8 @@ public class ButterflyPrint {
                 System.out.print(j);
             }
             System.out.println();
-
+// лишняя строка, удали
         }
-
+// лишняя строка, удали
     }
 }
